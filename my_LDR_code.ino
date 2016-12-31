a6b71640957a2b53a87e172bdf35d0d46a83e75c
Maitm29 <maitreyam29@gmail.com>
2016-12-31 20:49:26 +0530

    start
    to control the intensity of light using pwm of arduninouno

---
+++ my_LDR_code.ino
@@ -0,0 +1,18 @@
+int inputpin = A0;
+int inputvalue = 0;
+
+void setup() 
+{ pinMode (2,OUTPUT);
+Serial.begin(9600);
+
+}
+
+void loop() 
+{ inputvalue = analogRead(inputpin);
+Serial.println(inputvalue);
+
+if(inputvalue < 800)
+digitalWrite(2,HIGH);
+else 
+digitalWrite(2,LOW);
+}
